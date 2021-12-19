e971d6e0a20a01f7d450102106c92e636ce57462
masxxiii <masxafar@gmail.com>
2021-12-19 17:57:44 +0700

    Add brand model

---
+++ server/src/models/brand.ts
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+import { sequelizeDB } from "../config/sequelize";
+
+export const Brand = sequelizeDB.define('Brand', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    Name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    country: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    rating: {
+        type: DataTypes.NUMBER,
+        allowNull: false
+    },
+},{
+    timestamps: false
+})
