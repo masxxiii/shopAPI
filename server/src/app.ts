e8b792d494d4a784ea30940a13ee3e6e78da6523
masxxiii <masxafar@gmail.com>
2021-12-19 19:33:18 +0700

    add routes and configure swagger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,9 @@
 'use strict';
 const Hapi = require('@hapi/hapi');
+const HapiSwagger = require('hapi-swagger');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const Pack = require('../../package.json');
 const { createDatabase, dropDatabase } = require("./config/database");
 import { connectSequelize, disconnectSequelize } from "./config/sequelize";
 import { createTables } from "./config/tables";
@@ -16,17 +20,34 @@ const startServer = async () => {
         host: 'localhost'
     });
 
+    //adding routes
+    server.route(routes);
+
+    //adding swagger
+    const swaggerOptions = {
+        info: {
+            title: 'Shop API Documentation',
+            version: Pack.version
+        },
+    };
+
+    //Register all the plugins
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
     await server.start();
     console.log('[Server running on %s]', server.info.uri);
 
     //Internal function calls
     // await createDatabase();
+    // await createTables();
     await connectSequelize();
-    await createTables();
-
-    //adding routes
-    server.route(routes);
-
 };
 
 /**
