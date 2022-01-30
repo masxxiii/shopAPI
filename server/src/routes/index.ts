1ae5dd01266e967e30b12a7e9d6730186104de73
Masood <57627236+masxxiii@users.noreply.github.com>
2022-01-30 18:38:43 +0700

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,1 @@
-'use strict';
-import getRoute from "./getRoute";
-import postRoute from "./postRoute";
-import updateRoute from "./updateRoute";
-import deleteRoute from "./deleteRoute";
 
-export default [
-    ...getRoute,
-    ...postRoute,
-    ...updateRoute,
-    ...deleteRoute,
-];
