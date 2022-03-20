0d0034a6946844124110b5f83678cef3e303cee7
Masood <57627236+masxxiii@users.noreply.github.com>
2022-03-21 00:10:49 +0700

    Update order.ts

--- server/src/models/order.ts
+++ server/src/models/order.ts
@@ -2,7 +2,12 @@ const { DataTypes } = require('sequelize');
 import { sequelizeDB } from "../config/sequelize";
 
 export const Order = sequelizeDB.define('Order', {
-
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
     EmployeeId: {
         type: DataTypes.UUID,
         allowNull: false,
